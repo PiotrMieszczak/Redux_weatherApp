fdfc820a6dfe6d8fcf1248b92fcdc9acecd657f0
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-08-21 22:30:01 +0200

    first commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class Booklist extends React.Component{
+
+    renderList(){
+        return this.props.books.map( book=>{
+            return <li key={book.title} className="list-group-item">{book.title}</li>
+        })
+    }
+
+    render(){
+        return <ul className="list-group col-sm-4">
+            {this.renderList()}
+        </ul>
+    }
+}
